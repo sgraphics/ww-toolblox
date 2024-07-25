063174ce66ea8a623d0a39507d81aa11825cf3ad
Silver Sepp <silver.sepp@gmail.com>
2024-07-25 12:11:00 +0300

    editor remove

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -19,24 +19,26 @@
 
 <script>
 window.web3Initialized = false;
-// if (window.Web3AuthNoModal == null || window.Web3AuthNoModal == undefined) {
-//   (async () => {
-//     try {
-//       const { Web3AuthNoModal } = await import('@web3auth/no-modal');
-//       const { EthereumPrivateKeyProvider } = await import('@web3auth/ethereum-provider');
-//       const { OpenloginAdapter } = await import('@web3auth/openlogin-adapter');
-//       const { CHAIN_NAMESPACES, WEB3AUTH_NETWORK } = await import('@web3auth/base');
+/* wwEditor:start */
+if (window.Web3AuthNoModal == null || window.Web3AuthNoModal == undefined) {
+  (async () => {
+    try {
+      const { Web3AuthNoModal } = await import('@web3auth/no-modal');
+      const { EthereumPrivateKeyProvider } = await import('@web3auth/ethereum-provider');
+      const { OpenloginAdapter } = await import('@web3auth/openlogin-adapter');
+      const { CHAIN_NAMESPACES, WEB3AUTH_NETWORK } = await import('@web3auth/base');
 
-//       window.Web3AuthNoModal = Web3AuthNoModal;
-//       window.EthereumPrivateKeyProvider = EthereumPrivateKeyProvider;
-//       window.CHAIN_NAMESPACES = CHAIN_NAMESPACES;
-//       window.WEB3AUTH_NETWORK = WEB3AUTH_NETWORK;
-//       window.OpenloginAdapter = OpenloginAdapter;
-//     } catch (err) {
-//       //console.error('Failed to load Web3Auth modules', err);
-//     }
-//   })();
-// }
+      window.Web3AuthNoModal = Web3AuthNoModal;
+      window.EthereumPrivateKeyProvider = EthereumPrivateKeyProvider;
+      window.CHAIN_NAMESPACES = CHAIN_NAMESPACES;
+      window.WEB3AUTH_NETWORK = WEB3AUTH_NETWORK;
+      window.OpenloginAdapter = OpenloginAdapter;
+    } catch (err) {
+      //console.error('Failed to load Web3Auth modules', err);
+    }
+  })();
+}
+/* wwEditor:end */
 
 const TEXT_ALIGN_TO_JUSTIFY = {
     center: 'center',
@@ -62,184 +64,187 @@ export default {
         'remove-state',
         'trigger-event',
     ],
-    /* wwEditor:start */
     setup() {
-        // function getQueryParams(url) {
-        //     let params = {};
-        //     let parser = document.createElement('a');
-        //     parser.href = url;
-        //     let query = parser.search.substring(1);
-        //     let vars = query.split('&');
-        //     for (let i = 0; i < vars.length; i++) {
-        //         let pair = vars[i].split('=');
-        //         params[pair[0]] = decodeURIComponent(pair[1]);
-        //     }
-        //     return params;
-        // }
+        function getQueryParams(url) {
+            let params = {};
+            let parser = document.createElement('a');
+            parser.href = url;
+            let query = parser.search.substring(1);
+            let vars = query.split('&');
+            for (let i = 0; i < vars.length; i++) {
+                let pair = vars[i].split('=');
+                params[pair[0]] = decodeURIComponent(pair[1]);
+            }
+            return params;
+        }
 
-        // // Get query parameters
-        // let queryParams = getQueryParams(window.location.href);
-        // // Extract oauth_token and oauth_verifier
-        // let oauthToken = queryParams['oauth_token'];
-        // let oauthVerifier = queryParams['oauth_verifier'];
-        // const fetchJWTFromXano = async (oauthToken, oauthVerifier) => {
-        //     const response = await fetch('https://xsrr-l2ye-dpbj.f2.xano.io/api:NUVFj-l-:Web3Auth/oauth/twitter/access_token', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         },
-        //         body: JSON.stringify({
-        //             oauth_token: oauthToken,
-        //             oauth_verifier: oauthVerifier
-        //         })
-        //     });
-        //     const data = await response.json();
-        //     //alert(JSON.stringify(data));
-        //     return data.authToken; // Assuming the JWT is in the `token` field
-        // };
+        // Get query parameters
+        let queryParams = getQueryParams(window.location.href);
+        // Extract oauth_token and oauth_verifier
+        let oauthToken = queryParams['oauth_token'];
+        let oauthVerifier = queryParams['oauth_verifier'];
+        const fetchJWTFromXano = async (oauthToken, oauthVerifier) => {
+            const response = await fetch('https://xsrr-l2ye-dpbj.f2.xano.io/api:NUVFj-l-:Web3Auth/oauth/twitter/access_token', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    oauth_token: oauthToken,
+                    oauth_verifier: oauthVerifier
+                })
+            });
+            const data = await response.json();
+            //alert(JSON.stringify(data));
+            return data.authToken; // Assuming the JWT is in the `token` field
+        };
 
-        // // Define the function to authenticate with Web3Auth
-        // const authenticateWithWeb3Auth = async (web3auth, jwt) => {
-        //     try {
-        //         await web3auth.logout();
-        //         console.log('Successfully logged out');
-        //     } catch (error) {
-        //         console.error('Logout failed:', error);
-        //     }
+        // Define the function to authenticate with Web3Auth
+        const authenticateWithWeb3Auth = async (web3auth, jwt) => {
+            try {
+                await web3auth.logout();
+                console.log('Successfully logged out');
+            } catch (error) {
+                console.error('Logout failed:', error);
+            }
 
-        //     await web3auth.connectTo("openlogin", {
-        //         loginProvider: "jwt",
-        //         extraLoginOptions: {
-        //             id_token: jwt,
-        //             verifierIdField: "sub",
-        //             domain: "https://your-domain.com", // Your domain if required
-        //         },
-        //     });
+            await web3auth.connectTo("openlogin", {
+                loginProvider: "jwt",
+                extraLoginOptions: {
+                    id_token: jwt,
+                    verifierIdField: "sub",
+                    domain: "https://your-domain.com", // Your domain if required
+                },
+            });
 
-        //     return web3auth;
-        // };
-        // // Define the function to authenticate with Web3Auth
-        // const initWeb3Auth = async () => {
-        //     const chainConfig = {
-        //         chainNamespace: window.CHAIN_NAMESPACES.EIP155,
-        //         chainId: "0x1",
-        //         displayName: "Ethereum Mainnet",
-        //         rpcTarget: "https://rpc.ankr.com/eth",
-        //         blockExplorerUrl: "https://etherscan.io",
-        //         ticker: "ETH",
-        //         tickerName: "Ethereum",
-        //         logo: "https://web3auth.io/images/web3authlog.png",
-        //     };
+            return web3auth;
+        };
+        // Define the function to authenticate with Web3Auth
+        const initWeb3Auth = async () => {
+            const chainConfig = {
+                chainNamespace: window.CHAIN_NAMESPACES.EIP155,
+                chainId: "0x1",
+                displayName: "Ethereum Mainnet",
+                rpcTarget: "https://rpc.ankr.com/eth",
+                blockExplorerUrl: "https://etherscan.io",
+                ticker: "ETH",
+                tickerName: "Ethereum",
+                logo: "https://web3auth.io/images/web3authlog.png",
+            };
 
-        //     const privateKeyProvider = new window.EthereumPrivateKeyProvider({
-        //         config: { chainConfig },
-        //     });
-        //     var clientId = "BCD3bVaYA3ZHu3F0bEs6eEyk3OzC1zZvTwkFkFQsiicid-7H4bxyQgBlC9IxCipUYlEbQ1P6ZqBzuDBAMx7svIA";
+            const privateKeyProvider = new window.EthereumPrivateKeyProvider({
+                config: { chainConfig },
+            });
+            var clientId = "BCD3bVaYA3ZHu3F0bEs6eEyk3OzC1zZvTwkFkFQsiicid-7H4bxyQgBlC9IxCipUYlEbQ1P6ZqBzuDBAMx7svIA";
 
-        //     const web3AuthOptions = {
-        //         clientId,
-        //         web3AuthNetwork: window.WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
-        //         privateKeyProvider,
-        //     };
+            const web3AuthOptions = {
+                clientId,
+                web3AuthNetwork: window.WEB3AUTH_NETWORK.SAPPHIRE_DEVNET,
+                privateKeyProvider,
+            };
 
-        //     const web3auth = new window.Web3AuthNoModal(web3AuthOptions);
+            const web3auth = new window.Web3AuthNoModal(web3AuthOptions);
 
-        //     const openloginAdapter = new window.OpenloginAdapter({
-        //         adapterSettings: {
-        //             clientId: clientId, // Replace with your Web3Auth client ID
-        //             network: "sapphire_devnet", // Ensure this matches your network
-        //             uxMode: "redirect",
-        //             redirectUrl: window.location.href,
-        //             loginConfig: {
-        //                 jwt: {
-        //                     name: "Twitter Login", // Display name
-        //                     verifier: "rddtor-verifier",
-        //                     verifierSubIdentifier: "xano-twitter-rddtor-verifier",
-        //                     typeOfLogin: "jwt",
-        //                     clientId: clientId,
-        //                 },
-        //             },
-        //         },
-        //     });
+            const openloginAdapter = new window.OpenloginAdapter({
+                adapterSettings: {
+                    clientId: clientId, // Replace with your Web3Auth client ID
+                    network: "sapphire_devnet", // Ensure this matches your network
+                    uxMode: "redirect",
+                    redirectUrl: window.location.href,
+                    loginConfig: {
+                        jwt: {
+                            name: "Twitter Login", // Display name
+                            verifier: "rddtor-verifier",
+                            verifierSubIdentifier: "xano-twitter-rddtor-verifier",
+                            typeOfLogin: "jwt",
+                            clientId: clientId,
+                        },
+                    },
+                },
+            });
 
-        //     web3auth.configureAdapter(openloginAdapter);
+            web3auth.configureAdapter(openloginAdapter);
             
-        //     await web3auth.init();
+            await web3auth.init();
 
-        //     return web3auth;
-        // };
+            return web3auth;
+        };
 
-        // // Define the function to send the Web3Auth token to Xano
-        // const sendWeb3AuthTokenToXano = async (web3auth) => {
-        //     const authenticationResult = await web3auth.authenticateUser();
+        // Define the function to send the Web3Auth token to Xano
+        const sendWeb3AuthTokenToXano = async (web3auth) => {
+            const authenticationResult = await web3auth.authenticateUser();
             
-        //     // prompt("sdasd", authenticationResult.idToken);
-        //     const web3authToken = authenticationResult.idToken; // Replace with the correct way to get the token from provider
-        //     const response = await fetch('https://xsrr-l2ye-dpbj.f2.xano.io/api:NUVFj-l-:Web3Auth/oauth/web3auth/authenticate', {
-        //         method: 'POST',
-        //         headers: {
-        //             'Content-Type': 'application/json'
-        //         },
-        //         body: JSON.stringify({
-        //             token: web3authToken
-        //         })
-        //     });
-        //     const data = await response.json();
-        //     alert("Wallet created! \r\n" + JSON.stringify(data));
-        //     return data;
-        // };
+            // prompt("sdasd", authenticationResult.idToken);
+            const web3authToken = authenticationResult.idToken; // Replace with the correct way to get the token from provider
+            const response = await fetch('https://xsrr-l2ye-dpbj.f2.xano.io/api:NUVFj-l-:Web3Auth/oauth/web3auth/authenticate', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    token: web3authToken
+                })
+            });
+            const data = await response.json();
+            alert("Wallet created! \r\n" + JSON.stringify(data));
+            return data;
+        };
 
-        // // Main async function to handle the login process
-        // const handleLogin = async (oauthToken, oauthVerifier) => {
-        //     const web3auth = await initWeb3Auth();
-        //     if (web3auth.connected)
-        //     {
-        //         try {
-        //             //await web3auth.connect();
-        //             const result = await sendWeb3AuthTokenToXano(web3auth);
-        //             console.log("Login successful:", result);
-        //         } catch (error) {
-        //             console.error("Login failed:", error);
-        //         }
-        //     }
-        //     if (oauthToken && oauthVerifier){                
-        //         try {
-        //             const jwt = await fetchJWTFromXano(oauthToken, oauthVerifier);
-        //             await authenticateWithWeb3Auth(web3auth, jwt);
-        //         } catch (error) {
-        //             console.error("Starting login failed:", error);
-        //         }
-        //     }
-        // };
+        // Main async function to handle the login process
+        const handleLogin = async (oauthToken, oauthVerifier) => {
+            const web3auth = await initWeb3Auth();
+            if (web3auth.connected)
+            {
+                try {
+                    //await web3auth.connect();
+                    const result = await sendWeb3AuthTokenToXano(web3auth);
+                    console.log("Login successful:", result);
+                } catch (error) {
+                    console.error("Login failed:", error);
+                }
+            }
+            if (oauthToken && oauthVerifier){                
+                try {
+                    const jwt = await fetchJWTFromXano(oauthToken, oauthVerifier);
+                    await authenticateWithWeb3Auth(web3auth, jwt);
+                } catch (error) {
+                    console.error("Starting login failed:", error);
+                }
+            }
+        };
 
-        // // Perform actions if both oauth_token and oauth_verifier are set
-        // if (oauthToken && oauthVerifier) {
-        //     function removeUrlParameters(url, parameters) {
-        //         let urlObj = new URL(url);
-        //         parameters.forEach(param => urlObj.searchParams.delete(param));
-        //         return urlObj.toString();
-        //     }
-        //     // Get the current URL
-        //     const currentUrl = window.location.href;
-        //     // Parameters to remove
-        //     const paramsToRemove = ['oauth_token', 'oauth_verifier'];
-        //     // Get the new URL without the specified parameters
-        //     const newUrl = removeUrlParameters(currentUrl, paramsToRemove);
-        //     // Replace the current history state with the new URL
-        //     window.history.replaceState({}, document.title, newUrl);
-        // }
-        // alert(1);
-        // if (window.web3Initialized == false)
-        // {
-        //     alert(2);
-        //     window.web3Initialized = true;
-        //     debug(handleLogin(oauthToken, oauthVerifier));
-        // }
+        // Perform actions if both oauth_token and oauth_verifier are set
+        if (oauthToken && oauthVerifier) {
+            function removeUrlParameters(url, parameters) {
+                let urlObj = new URL(url);
+                parameters.forEach(param => urlObj.searchParams.delete(param));
+                return urlObj.toString();
+            }
+            // Get the current URL
+            const currentUrl = window.location.href;
+            // Parameters to remove
+            const paramsToRemove = ['oauth_token', 'oauth_verifier'];
+            // Get the new URL without the specified parameters
+            const newUrl = removeUrlParameters(currentUrl, paramsToRemove);
+            // Replace the current history state with the new URL
+            window.history.replaceState({}, document.title, newUrl);
+        }
+        if (window.web3Initialized == false)
+        {
+            window.web3Initialized = true;
+            debug(handleLogin(oauthToken, oauthVerifier));
+        }
 
+        /* wwEditor:start */
         const { createElement } = wwLib.useCreateElement();
-        return { createElement };
+        /* wwEditor:end */
+
+        return {
+            /* wwEditor:start */
+            createElement,
+            /* wwEditor:end */
+        };
     },
-    /* wwEditor:end */
     data() {
         return {
             isReallyFocused: false,
@@ -358,6 +363,7 @@ export default {
                 });
             }
         },
+        /* wwEditor:end */
         xLogin() {
             
             const fetchRequestUrlFromXano = async () => {
@@ -372,20 +378,15 @@ export default {
             const startXLogin = async () => {
                 try {
                     
-            alert(2);
                     const authUrl = await fetchRequestUrlFromXano();
                     
-            alert(authUrl);
                     window.location.href = authUrl;
                 } catch (error) {
                     console.error("Login failed:", error);
-                    alert(error);
                 }
             };
-            alert(1);
             startXLogin();
         },
-        /* wwEditor:end */
         onBlur() {
             this.isReallyFocused = false;
         },
